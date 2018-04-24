2ea9272f6a86207289d4417a1e64d0ea11273c53
Adam Hopkins <admhpkns@gmail.com>
2018-04-24 23:35:59 +0300

    New tests based on issues discussions

---
+++ tests/test_wrong_token.py
@@ -0,0 +1,21 @@
+import jwt
+
+
+def test_wrong_token(app):
+    sanic_app, sanic_jwt = app
+
+    payload = {"foo": "bar"}
+
+    access_token = jwt.encode(
+        payload, sanic_jwt.config.secret(), algorithm=sanic_jwt.config.algorithm()
+    ).decode(
+        "utf-8"
+    )
+
+    _, response = sanic_app.test_client.get(
+        "/protected",
+        headers={"Authorization": "Bearer {}".format(access_token)},
+    )
+
+    print(response.json)
+    assert response.status == 200

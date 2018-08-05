f552e205ecd63003c34aad366da0c1da0ceb52db
Adam Hopkins <admhpkns@gmail.com>
2018-08-05 15:29:00 +0300

    #110 test

---
+++ tests/test_async_options.py
@@ -0,0 +1,83 @@
+"""
+Test code taken from:
+https://github.com/ahopkins/sanic-jwt/issues/110#issue-330031252
+"""
+
+
+from sanic.views import HTTPMethodView
+from sanic.response import text
+from sanic_jwt import Authentication, initialize, protected
+from sanic import Sanic, Blueprint
+
+import pytest
+
+
+ALL_METHODS = ['GET', 'OPTIONS']
+
+
+class TestMethodView(HTTPMethodView):
+    async def options(self, *args, **kwargs):
+        return text('ok')
+
+
+class Tester(TestMethodView):
+    decorators = [protected()]
+
+    async def get(self, request):
+        return text('ok')
+
+
+bp = Blueprint('bp')
+bp.add_route(Tester.as_view(), '/test', methods=ALL_METHODS)
+
+
+class CustomAuth(Authentication):
+    async def authenticate(self, request, *args, **kwargs):
+        return {'username': 'Rich', 'password': 'not secure'}
+
+    async def retrieve_user(self, request, payload, *args, **kwargs):
+        if payload:
+            user_id = payload.get('username', None)
+            passwd = payload.get('password', None)
+            return {'username': user_id, 'password': passwd}
+
+    async def extend_payload(self, payload, user=None, *args, **kwargs):
+        if user:
+            payload.update({'extra_info': 'awesome!'})
+        return payload
+
+
+@pytest.fixture
+def app():
+    app = Sanic(__name__)
+    app.config.SANIC_JWT_AUTHORIZATION_HEADER_PREFIX = 'JWT'
+    app.config.SANIC_JWT_EXPIRATION_DELTA = 360000
+    app.config.SANIC_JWT_USER_ID = 'username'
+
+    sanicjwt = initialize(app, authentication_class=CustomAuth)
+    app.blueprint(bp)
+
+    return app, sanicjwt
+
+
+def test_async_options(app):
+    sanic_app, sanicjwt = app
+    _, response = sanic_app.test_client.get("/test")
+    assert response.status == 401
+
+    _, response = sanic_app.test_client.post(
+        "/auth", json={"username": "Rich", "password": "not secure"}
+    )
+
+    access_token = response.json.get(
+        sanicjwt.config.access_token_name(), None
+    )
+
+    assert response.status == 200
+    assert access_token is not None
+
+    _, response = sanic_app.test_client.options("/test")
+    print(response.body)
+
+    assert response.status == 200
+    assert response.body == b'ok'

e05094b97589907556bbbb93593c5890914506ce
Jekel <jekel@psyspace.ru>
2022-11-28 12:48:21 +0100

    fixed tests

--- tests/test_endpoints_scoped_simple.py
+++ tests/test_endpoints_scoped_simple.py
@@ -1,22 +1,28 @@
+import pytest
 from sanic import Sanic
 from sanic.response import json
 
+import sanic_jwt
 from sanic_jwt import initialize
-from sanic_jwt.decorators import scoped
-
-app = Sanic("sanic-jwt-test")
-initialize(app, authenticate=lambda: True)
-
-
-@app.route("/scoped_empty")
-@scoped("something")
-async def scoped(request):
-    return json({"scoped": True})
 
 
 class TestEndpointsScoped(object):
-    def test_scoped_empty(self):
-        _, response = app.test_client.get("/scoped_empty")
+    @pytest.fixture
+    def app_with_simple_scope(self):
+        sanic_app = Sanic("sanic-jwt-test")
+        sanicjwt = initialize(sanic_app, authenticate=lambda: True)
+
+        @sanic_app.route("/scoped_empty")
+        @sanic_jwt.scoped("something")
+        async def scoped(request):
+            return json({"scoped": True})
+
+        yield sanic_app, sanicjwt
+
+    def test_scoped_empty(self, app_with_simple_scope):
+        sanic_app, _ = app_with_simple_scope
+
+        _, response = sanic_app.test_client.get("/scoped_empty")
         assert response.status == 401
         assert response.json.get("exception") == "Unauthorized"
         assert "Authorization header not present." in response.json.get(
